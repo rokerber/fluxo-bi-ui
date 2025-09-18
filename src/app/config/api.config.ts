fa1165f485ad18c26c5c93d0b2007926ff00c8db
kerber <kerber@localhost>
2025-09-18 19:44:50 +0200

    Fix url API Client

--- src/app/config/api.config.ts
+++ src/app/config/api.config.ts
@@ -14,7 +14,7 @@ const getCurrentEnvironment = (): 'dev' | 'staging' | 'local' => {
 const API_URLS = {
   dev: 'https://assign-humidity-districts-apollo.trycloudflare.com',
   staging: 'https://sonic-enforcement-alpine-hydrocodone.trycloudflare.com',
-  local: 'http://localhost:8080'
+  local: 'ttps://fluxo-bi-api.sp1.br.saveincloud.net.br'
 };
 
 
